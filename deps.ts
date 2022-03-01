2d273e8036bd4c5e00bfefc0703f8071556f0c57
Blake Chambers <chambb1@gmail.com>
2022-03-01 08:09:50 -0500

    dep update

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.122.0/flags/mod.ts";
-export { resolve } from "https://deno.land/std@0.122.0/path/mod.ts";
-export { expandGlob } from "https://deno.land/std@0.122.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@0.127.0/flags/mod.ts";
+export { resolve } from "https://deno.land/std@0.127.0/path/mod.ts";
+export { expandGlob } from "https://deno.land/std@0.127.0/fs/mod.ts";
 
 export { ensureString } from "../process-cli/shared/util.ts";
 
