bf414451cd0125234db64906ffa7e1613638b4ce
Blake Chambers <chambb1@gmail.com>
2023-02-21 22:36:22 -0500

    update deps

--- deps.ts
+++ deps.ts
@@ -2,11 +2,11 @@ export { parse } from "https://deno.land/std@0.127.0/flags/mod.ts";
 export { resolve } from "https://deno.land/std@0.127.0/path/mod.ts";
 export { expandGlob } from "https://deno.land/std@0.127.0/fs/mod.ts";
 
-export { ensureString } from "../process-cli/shared/util.ts";
+export { ensureString } from "../_cabinet/process-cli/shared/util.ts";
 
 export {
   buildHelpMessage,
   displayTwoColumnList,
   formatBlockList,
   withIndent,
-} from "../process-cli/shared/helpMessage.ts";
+} from "../_cabinet/process-cli/shared/helpMessage.ts";
